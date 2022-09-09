9f24b27039195db6623777b6988ab0e03cd26374
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-09-09 14:55:04 -0500

    first commit

---
+++ Test/01-ContarOvejas.test.js
@@ -0,0 +1,9 @@
+const {contarOvejas} = require("../Exercises/01-ContarOvejas.js")
+
+describe("Excercise 1 ; Contar ovejas",()=>{
+    it("should be a list of sheeps",()=>{
+        expect(contarOvejas("hola")).toBe("")
+    })
+})
+
+// gente se pueden registrar con su cuenta educativa y tienen github copilot gratis
