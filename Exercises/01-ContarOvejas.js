8e2d3456a7fcb9ade529e9d9962c27047a0657f1
Your Namesergio-jc <sergiojara0609@gmail.com>
2022-09-09 15:28:14 -0500

    test: add test contarOvejas

--- Exercises/01-ContarOvejas.js
+++ Exercises/01-ContarOvejas.js
@@ -1,9 +1,11 @@
 
 function contarOvejas (ovejas) {
     // aquí tu magia
-    if(typeof ovejas === "undefined") return "Enter a parameter,please.";
-    if( ovejas === null || 1) return "This is not a list of sheeps";
-    
+    if(typeof ovejas === "undefined") return  "Enter a parameter,please.";
+    if( typeof ovejas !== "object" ) return "This is not a list of sheeps";
+    if (ovejas === null ) return "null is being received as a parameter" ;
+    if (!ovejas[0]) return "There are no sheep in this list" ;
+
     let redSheepAm = ovejas.filter(sheeps => {
       return (
       sheeps.name.toUpperCase().includes("A") &&
--- Test/01-ContarOvejas.test.js
+++ Test/01-ContarOvejas.test.js
@@ -1,8 +1,28 @@
 const {contarOvejas} = require("../Exercises/01-ContarOvejas.js")
 
 describe("Excercise 1 ; Contar ovejas",()=>{
+    it("you must enter an argument",()=>{
+        expect(contarOvejas()).toBe("Enter a parameter,please.");
+    })
     it("should be a list of sheeps",()=>{
         expect(contarOvejas("hola")).toBe("This is not a list of sheeps")
+        expect(contarOvejas(5)).toBe("This is not a list of sheeps")
+        expect(contarOvejas(null)).toBe("null is being received as a parameter")
+    })
+    it("should return a list of sheep that are red and have A or N in their names.",()=>{
+        const ovejas = [
+            { name: 'Noa', color: 'azul' },
+            { name: 'Euge', color: 'rojo' },
+            { name: 'Navidad', color: 'rojo' },
+            { name: 'Ki Na Ma', color: 'rojo'},
+            { name: 'AAAAAaaaaa', color: 'rojo' },
+            { name: 'Nnnnnnnn', color: 'rojo'}
+          ]
+        const result = [
+            { name: 'Navidad', color: 'rojo' },
+            { name: 'Ki Na Ma', color: 'rojo' }
+          ]
+        expect(contarOvejas(ovejas)).toEqual(result)
     })
 })
 
